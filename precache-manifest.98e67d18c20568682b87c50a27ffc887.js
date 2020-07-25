2ac296ee12785c41fa0793e06c7ac491f855ec75
Jeeson Domiguez <dominguez_js@outlook.com>
2020-07-25 01:24:43 -0600

    Updates

---
+++ precache-manifest.98e67d18c20568682b87c50a27ffc887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20e3992e58a1321ee0bbd9b489c79bc",
+    "url": "/react-cryptcoinsTrader/index.html"
+  },
+  {
+    "revision": "fdefcd1fc70593b52166",
+    "url": "/react-cryptcoinsTrader/static/css/main.a7eaaa4d.chunk.css"
+  },
+  {
+    "revision": "edea70031e00d3838600",
+    "url": "/react-cryptcoinsTrader/static/js/2.aa219e80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cryptcoinsTrader/static/js/2.aa219e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdefcd1fc70593b52166",
+    "url": "/react-cryptcoinsTrader/static/js/main.3118ce4b.chunk.js"
+  },
+  {
+    "revision": "3d4ccd7ee1f0dc2a60b9",
+    "url": "/react-cryptcoinsTrader/static/js/runtime-main.b9865bf7.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/react-cryptcoinsTrader/static/media/cryptocoins.96e45d69.png"
+  }
+]);
